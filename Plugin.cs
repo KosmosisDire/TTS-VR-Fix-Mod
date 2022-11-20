3046b2b4e82494854f8e6e1cadbc0b09c8cfa7bc
Nathan George <kosmosisdire@gmail.com>
2022-11-20 15:40:10 -0500

    first commit

---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace VRImprovements;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("Tabletop Simulator.exe")]
+public class Plugin : BaseUnityPlugin
+{
+    Transform floor;
+
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private void Update()
+    {
+        // hide VR floor
+        if (!floor)
+        {
+            floor = GameObject.Find("[VR]/[Static]/Floor").transform;
+            floor.GetComponent<MeshRenderer>().enabled = false;
+        }
+        
+    }
+}
---
+++ obj/Debug/netstandard2.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace VRImprovements
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "VRImprovements";
+        public const string PLUGIN_NAME = "VRImprovements";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
